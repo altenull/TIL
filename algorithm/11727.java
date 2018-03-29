15915034420801e64600a415c4ab5c7c29e9f8b3
altenull <altenull@gmail.com>
2018-03-29 15:47:24 +0900

    11727번 2xn 타일링2(DP 관련)

---
+++ algorithm/11727.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  // DP - Bottom-up 방식
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] tileCase = new int[1001];
+    tileCase[1] = 1;
+    tileCase[2] = 3;
+    for ( int i = 3;  i <= n;  i++ ) {
+      tileCase[i] = tileCase[i - 1] + (2 * tileCase[i - 2]);
+      tileCase[i] %= 10007;
+    }
+    System.out.println(tileCase[n]);
+  }
+}
